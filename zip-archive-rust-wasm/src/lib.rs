d7eb419783e02f5138fcbdb9f5e78362cb412943
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-11-17 21:16:18 +0900

    Do not use deserialization

--- zip-archive-rust-wasm/src/lib.rs
+++ zip-archive-rust-wasm/src/lib.rs
@@ -1,58 +1,63 @@
-use serde::Deserialize;
-use std::io::{Cursor, Write};
 use wasm_bindgen::prelude::*;
+use js_sys::{Array, Uint8Array};
+use std::io::Write;
+use zip::{ZipWriter, write::FileOptions};
+use std::io::Cursor;
 use web_sys::console::log_1;
-use zip::write::FileOptions;
 
-#[derive(Default, Deserialize)]
-struct Data {
-    name: String,
-    value: Vec<u8>,
+fn zip_error_to_js_value<E: std::fmt::Debug>(e: E) -> JsValue {
+    JsValue::from_str(&format!("Zip error: {:?}", e))
 }
 
 #[wasm_bindgen]
-pub fn archive(val: JsValue) {
-    let data: Vec<Data> = serde_wasm_bindgen::from_value(val).unwrap();
-    let mut bytes: Vec<u8> = Vec::new();
-    {
-    let mut cursor = Cursor::new(&mut bytes);
-    let mut zip = zip::ZipWriter::new(&mut cursor);
-    let options = FileOptions::default()
+pub fn archive(files: Array) -> Result<(), JsValue> {
+    let mut zip_buffer = Cursor::new(Vec::new());
+    let mut zip = ZipWriter::new(&mut zip_buffer);
+    let options: FileOptions<'static, ()> = FileOptions::default()
         .compression_method(zip::CompressionMethod::Deflated)
-        .compression_level(Some(9))
-        .unix_permissions(0o755);
-    zip.add_directory("zip_result/", options).unwrap();
- 
-    for v in data {
-        zip.start_file(format!("zip_result/{}", v.name), options).unwrap();
-        zip.write(&(v.value)[..]).unwrap();
-    }
-    zip.finish().unwrap();
+        .compression_level(Some(9));
+
+    for i in 0..files.length() {
+        let file = js_sys::Reflect::get(&files, &i.into())?;
+        let name = js_sys::Reflect::get(&file, &"name".into())?
+            .as_string()
+            .ok_or_else(|| JsValue::from_str("Invalid file name"))?;
+        let value = js_sys::Reflect::get(&file, &"value".into())?;
+        let data = Uint8Array::new(&value).to_vec();
+
+        zip.start_file(format!("zip_results/{}", &name), options).map_err(zip_error_to_js_value)?;
+        zip.write_all(&data).map_err(zip_error_to_js_value)?;
     }
 
-    download( bytes, "text/plain", "archive.zip");
+    zip.finish().map_err(zip_error_to_js_value)?;
+    download( zip_buffer.into_inner(), "application/zip", "archive.zip");
+    output_timestamp_log();
+    Ok(())
 }
 
-fn download(result_bytes: Vec<u8>, mime_type: &str, file_name: &str) {
-    let u8_array = js_sys::Uint8Array::new_with_length(result_bytes.len() as u32);
-    u8_array.copy_from(&result_bytes[..]);
+fn download(zip_data: Vec<u8>, mime_type: &str, file_name: &str) {
+    let u8_array = js_sys::Uint8Array::new_with_length(zip_data.len() as u32);
+    u8_array.copy_from(&zip_data[..]);
+    let blob_property_bag = web_sys::BlobPropertyBag::new();
+    blob_property_bag.set_type(mime_type);
+
     let blob_parts = js_sys::Array::new();
-    blob_parts.push(&u8_array.buffer());
-    let mut blob_property_bag = web_sys::BlobPropertyBag::new();
-    let blob = web_sys::Blob::new_with_u8_array_sequence_and_options(&blob_parts, blob_property_bag.type_(mime_type)).unwrap();
-    let downlad_url = web_sys::Url::create_object_url_with_blob(&blob).unwrap();
+    blob_parts.push(&JsValue::from(u8_array));
+
+    let blob = web_sys::Blob::new_with_u8_array_sequence_and_options(&blob_parts, &blob_property_bag).unwrap();
+
+    // BlobからオブジェクトURLを作成
+    let download_url = web_sys::Url::create_object_url_with_blob(&blob).unwrap();
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let a_elem = document.create_element("a").unwrap();
-    let _ = a_elem.set_attribute("href", &downlad_url);
+    let _ = a_elem.set_attribute("href", &download_url);
     let _ = a_elem.set_attribute("download", file_name);
 
-    let mouse_event = web_sys::MouseEvent::new("click").unwrap();
-    mouse_event.init_mouse_event_with_can_bubble_arg_and_cancelable_arg_and_view_arg(
-        "click", true, true, Some(&window));
-    let _ = a_elem.dispatch_event(&web_sys::Event::from(mouse_event));
-    output_timestamp_log();
+    // ダウンロードリンクをクリックするイベントを発火
+    let click_event = web_sys::MouseEvent::new("click").unwrap();
+    a_elem.dispatch_event(&click_event).unwrap();
 }
 
 fn output_timestamp_log() {
